e71224b1ee1993db22fd6481a7ebe6863dd2cb6b
Violeta Kukic <vkukic99@gmail.com>
2021-11-12 08:21:17 +0100

    login

--- src/main/java/app/isa/repository/UserRepository.java
+++ src/main/java/app/isa/repository/UserRepository.java
@@ -12,4 +12,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findOne(Long id);
     List<User> getAll();
+    Optional<User> findOneByEmail(String email);
 }
--- src/main/java/app/isa/security/RestAuthenticationEntryPoint.java
+++ src/main/java/app/isa/security/RestAuthenticationEntryPoint.java
@@ -1,2 +1,20 @@
-package app.isa.security;public @interface RestAuthenticationEntryPoint {
+package app.isa.security;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.security.auth.message.AuthException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+    }
+
 }
